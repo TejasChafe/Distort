61c8337d15ee8bf28f788ec6ffb2d8f9e47bc5ee
TejasChafe <tejaschafe@gmail.com>
2025-04-19 20:16:46 +0530

    pls

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin : 'https://distort-blond.vercel.app',
+    origin : '',
     methods : ['POST','GET','PUT','DELETE','OPTIONS'],
     credentials : true
 }))
